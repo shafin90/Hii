d24d1a30472cc63f5dd418f26642f574e9f22fcf
shafin90 <mashrafiahnam1@gmail.com>
2023-09-13 08:16:15 -0700

    push

--- app/Room/page.js
+++ app/Room/page.js
@@ -1,49 +1,37 @@
-'use client'
-
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
-import { data } from 'autoprefixer';
 
 const Room = () => {
     const [roomID, setRoomID] = useState('shafin');
+    const divRef = useRef(null);
 
-    let myMeeting = async (element) => {
+    useEffect(() => {
         const appID = 444743152;
         const serverSecret = "ba3fb655d906c4216afe0df2458bb9fb";
         const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomID, Date.now().toString(), "shafin");
 
-
         // Create instance object from Kit Token.
         const zp = ZegoUIKitPrebuilt.create(kitToken);
-        // start the call
-        zp.joinRoom({
-            container: element,
-            // sharedLinks: [
-            //     {
-            //         name: 'Personal link',
-            //         url:
-            //             window.location.protocol + '//' +
-            //             window.location.host + window.location.pathname +
-            //             '?roomID=' +
-            //             roomID,
-            //     },
-            // ],
-            scenario: {
-                mode: ZegoUIKitPrebuilt.OneONoneCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
-            },
-        });
 
+        // Configure the container element
+        const element = divRef.current;
 
+        if (element) {
+            zp.joinRoom({
+                container: element,
+                scenario: {
+                    mode: ZegoUIKitPrebuilt.OneONoneCall,
+                },
+            });
+        }
 
+        return () => {
+            // Clean up any resources when the component unmounts
+            zp.destroy();
+        };
+    }, [roomID]);
 
-    }
-
-
-    return (
-        <div ref={myMeeting} >
-            
-        </div>
-    );
+    return <div ref={divRef}></div>;
 };
 
 export default Room;
