fc39fb104bc2a919cd9d810790f7f701eb6c61fb
shafin90 <mashrafiahnam1@gmail.com>
2023-09-13 21:46:34 -0700

    push

--- app/Room/page.js
+++ app/Room/page.js
@@ -1,40 +1,56 @@
-'use client'
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
 
 const Room = () => {
+    const containerRef = useRef(null);
 
-    let myMeeting = async (element) => {
-        const appID = 444743152;
-        const serverSecret = "ba3fb655d906c4216afe0df2458bb9fb";
-        const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, "shafin", Date.now().toString(), "shafin");
+    useEffect(() => {
+        const myMeeting = async () => {
+            const appID = 444743152;
+            const serverSecret = "ba3fb655d906c4216afe0df2458bb9fb";
+            const roomName = "shafin"; // Replace with your desired room name
+            const userID = "shafin"; // Replace with your user ID or unique identifier
 
-        // Create instance object from Kit Token.
-        const zp = ZegoUIKitPrebuilt.create(kitToken);
-        zp.joinRoom({
-            container: element,
-            // sharedLinks: [
-            //   {
-            //     name: 'Personal link',
-            //     url:
-            //      window.location.protocol + '//' + 
-            //      window.location.host + window.location.pathname +
-            //       '?roomID=' +
-            //       roomID,
-            //   },
-            // ],
-            scenario: {
-                mode: ZegoUIKitPrebuilt.OneONoneCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
-            },
-        });
+            const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+                appID,
+                serverSecret,
+                roomName,
+                Date.now().toString(),
+                userID
+            );
 
+            // Create instance object from Kit Token.
+            const zp = ZegoUIKitPrebuilt.create(kitToken);
 
+            zp.init({
+                config: {
+                    logLevel: 'debug', // Adjust log level as needed
+                },
+            });
 
-    }
+            // You should set the element reference for rendering the video here.
+            zp.mount({
+                container: containerRef.current,
+            });
+
+            // Join the room
+            const joinResult = await zp.joinRoom(roomName);
+
+            if (joinResult) {
+                // Handle successful join
+            } else {
+                // Handle join failure
+            }
+        };
+
+        myMeeting();
+    }, []);
+
     return (
-        <div ref={myMeeting} className="flex min-h-screen flex-col items-center justify-between overflow-hidden p-24">
-
-        </div>
+        <div
+            ref={containerRef}
+            className="flex min-h-screen flex-col items-center justify-between overflow-hidden p-24"
+        ></div>
     );
 };
 
